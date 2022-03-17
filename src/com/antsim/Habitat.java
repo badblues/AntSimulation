ced48f5b0cb186407735c9d63bddc9330edfb197
Лёша Боровиков <borovikxv.a@gmail.com>
2022-03-17 22:14:50 +0700

    almost done

--- src/com/antsim/Habitat.java
+++ src/com/antsim/Habitat.java
@@ -2,16 +2,13 @@ package com.antsim;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -19,30 +16,25 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.util.Random;
-
-
-import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 //TODO simulation pause with continuation --------
 //TODO singleton
 //TODO divide window by two areas --------
-//TODO add Start and Stop buttons
+//TODO add Start and Stop buttons --------
 //TODO add switch Show info
-//TODO add group of two exclusive switches Show sim time and Hide sim time
-//TODO add main menu and tool panel
+//TODO add group of two exclusive switches Show sim time and Hide sim time --------
+//TODO add main menu and tool panel --------
 //TODO add modal dialog window with info about generated objects and time, window has 2 buttons OK and Cancel
 //TODo add control elements spawn delay and spawn chance
 
 
 public class Habitat extends Application {
 
-    static final int antWarriorSpawnChance = 50; // percents
-    static final int antWorkerSpawnChance = 50; // percents
-    static final int antWarriorSpawnDelay = 3; // seconds
-    static final int antWorkerSpawnDelay = 1; // seconds
+    static int antWarriorSpawnChance = 50; // percents
+    static int antWorkerSpawnChance = 50; // percents
+    static int antWarriorSpawnDelay = 1; // seconds
+    static int antWorkerSpawnDelay = 1; // seconds
     ArrayList<Ant> antsArray = new ArrayList<Ant>();
     int antWarriorCount = 0;
     int antWorkerCount = 0;
@@ -61,9 +53,7 @@ public class Habitat extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         try {
-
             stageSetUp(stage);
-
             scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
                 @Override
                 public void handle(KeyEvent keyEvent) {
@@ -74,7 +64,7 @@ public class Habitat extends Application {
                             break;
                         case E:
                             endTimer();
-                            showStatistic();
+                            //showStatistic();
                             break;
                         case T:
                             timeText.setVisible(!timeText.isVisible());
@@ -135,6 +125,18 @@ public class Habitat extends Application {
         statisticText.setVisible(true);
     }
 
+    private void showInfo(CheckBox button) {
+        endTimer();
+        Alert alert =  new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("STATISTICS");
+        alert.setHeaderText("Simulation time: " + time + "s\nWorkers born: " + antWorkerCount + "\nWarriors born :" + antWarriorCount);
+        alert.setContentText("End simulation?");
+        Optional<ButtonType> option = alert.showAndWait();
+        if (option.get() == ButtonType.CANCEL)
+            startTimer();
+        button.setSelected(false);
+    }
+
     private void stageSetUp(Stage stage) {
         scene.setFill(new Color(0.63, 0.39, 0.24, 1));
         stage.setScene(scene);
@@ -162,20 +164,25 @@ public class Habitat extends Application {
         root.getChildren().add(area);
 
         Button startButton = new Button("Start");
+        Button stopButton = new Button("Stop");
+
         startButton.setLayoutX(50);
         startButton.setLayoutY(50);
         startButton.setOnAction(actionEvent ->  {
             timer = startTimer();
             statisticText.setVisible(false);
+            startButton.setDisable(true);
+            stopButton.setDisable(false);
         });
         root.getChildren().add(startButton);
 
-        Button stopButton = new Button("Stop");
         stopButton.setLayoutX(100);
         stopButton.setLayoutY(50);
         stopButton.setOnAction(actionEvent -> {
             endTimer();
-            showStatistic();
+            stopButton.setDisable(true);
+            startButton.setDisable(false);
+            //showStatistic();
         });
         root.getChildren().add(stopButton);
 
@@ -199,5 +206,51 @@ public class Habitat extends Application {
         hideTimeButton.setToggleGroup(timeButtonsGroup);
         hideTimeButton.setSelected(true);
         root.getChildren().add(hideTimeButton);
+        
+        CheckBox showInfoButton = new CheckBox("Show info");
+        showInfoButton.setLayoutX(30);
+        showInfoButton.setLayoutY(140);
+        showInfoButton.setOnAction(actionEvent -> {
+            showInfo(showInfoButton);
+        });
+        root.getChildren().add(showInfoButton);
+
+        ObservableList<String> chances = FXCollections.observableArrayList("0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100");
+        ComboBox<String> antWarriorSpawnChanceBox = new ComboBox<String>(chances);
+        antWarriorSpawnChanceBox.setValue("50");
+        antWarriorSpawnChanceBox.setOnAction(event -> {
+            antWarriorSpawnChance = Integer.parseInt(antWarriorSpawnChanceBox.getValue());
+        });
+        antWarriorSpawnChanceBox.setLayoutX(30);
+        antWarriorSpawnChanceBox.setLayoutY(200);
+        root.getChildren().add(antWarriorSpawnChanceBox);
+
+        ComboBox<String> antWorkerSpawnChanceBox = new ComboBox<String>(chances);
+        antWorkerSpawnChanceBox.setValue("50");
+        antWorkerSpawnChanceBox.setOnAction(event -> {
+            antWorkerSpawnChance = Integer.parseInt(antWorkerSpawnChanceBox.getValue());
+        });
+        antWorkerSpawnChanceBox.setLayoutX(30);
+        antWorkerSpawnChanceBox.setLayoutY(250);
+        root.getChildren().add(antWorkerSpawnChanceBox);
+
+        Label text = new Label("Ant Warrior spawn chance");
+        text.setLayoutX(30);
+        text.setLayoutY(180);
+        root.getChildren().add(text);
+
+        Label text2 = new Label("Ant Worker spawn chance");
+        text2.setLayoutX(30);
+        text2.setLayoutY(230);
+        root.getChildren().add(text2);
+
+        Slider antWarriorTimeToSpawn = new Slider();
+        antWarriorTimeToSpawn.setMin(1);
+        antWarriorTimeToSpawn.setMax(100);
+        antWarriorTimeToSpawn.setMinorTickCount(1);
+        antWarriorTimeToSpawn.setOnDragDone(dragEvent -> {
+            antWarriorSpawnDelay = (int) antWarriorTimeToSpawn.getValue();
+        });
+        root.getChildren().add(antWarriorTimeToSpawn);
     }
 }
